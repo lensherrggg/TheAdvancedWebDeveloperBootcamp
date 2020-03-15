b8ccf7f4207c00a7be6c06271357d106482cb420
lensherrggg <gr98522@gmail.com>
2020-03-15 20:23:09 +0800

    Redux example

---
+++ Redux/example1/index.js
@@ -0,0 +1,43 @@
+// We need a reducer (rootReducer)
+// We need some redux store and initialState
+// We need some way of changing the state
+
+const initialState = {
+    count: 0
+};
+
+function rootReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            var newState = Object.assign({}, {count: state.count + 1})
+            return newState;
+        case 'DECREMENT':
+            var newState = Object.assign({}, {count: state.count - 1})
+            return newState;
+        default: 
+            return state;
+    }
+}
+
+const store = Redux.createStore(rootReducer);
+
+$(document).ready(function(){
+    let currentState = store.getState();
+    $("#counter").text(currentState.count);
+    $("#increment").on("click", function() {
+        // dispatch some action to increment the count!
+        store.dispatch({
+            type: 'INCREMENT'
+        });
+        let currentState = store.getState();
+        $("#counter").text(currentState.count);
+    })
+    $("#decrement").on("click", function() {
+        // dispatch some action to decrement the count!
+        store.dispatch({
+            type: 'DECREMENT'
+        })
+        let currentState = store.getState();
+        $("#counter").text(currentState.count);
+    })
+}) 

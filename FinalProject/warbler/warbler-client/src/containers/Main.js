48d35776fecf7e6184b264598d150bb6609c56d6
lensherrggg <gr98522@gmail.com>
2020-03-17 23:07:21 +0800

    format modified

--- FinalProject/warbler/warbler-client/src/containers/Main.js
+++ FinalProject/warbler/warbler-client/src/containers/Main.js
@@ -1,54 +1,54 @@
 import React from 'react';
-import {Switch, Route, withRouter, Redirect} from "react-router-dom";
-import {connect} from "react-redux";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
-import {authUser} from "../store/actions/auth";
+import { authUser } from "../store/actions/auth";
 
 const Main = props => {
-    const {authUser} = props;
-    return (
-        <div className="container">
-            <Switch>
-                <Route exact path="/" render={props => <Homepage {...props}/>} />
-                <Route 
-                    exact 
-                    path="/signin" 
-                    render={props => {
-                        return(
-                            <AuthForm 
-                                onAuth={authUser} 
-                                buttonText="Log in" 
-                                heading="Welcome Back." 
-                                {...props}
-                            />
-                        );
-                    }} 
-                />
-                <Route 
-                    exact 
-                    path="/signup" 
-                    render={props => {
-                        return(
-                            <AuthForm 
-                                onAuth={authUser} 
-                                signUp 
-                                buttonText="Sign me up!" 
-                                heading="Join Warbler today." 
-                                {...props}
-                            />
-                        );
-                    }} 
-                />
-            </Switch>
-        </div>
-    );
+  const { authUser } = props;
+  return (
+    <div className="container">
+      <Switch>
+        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                onAuth={authUser}
+                buttonText="Log in"
+                heading="Welcome Back."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                onAuth={authUser}
+                signUp
+                buttonText="Sign me up!"
+                heading="Join Warbler today."
+                {...props}
+              />
+            );
+          }}
+        />
+      </Switch>
+    </div>
+  );
 };
 
 function mapStateProps(state) {
-    return {
-        errors: state.errors
-    };
+  return {
+    errors: state.errors
+  };
 }
 
-export default withRouter(connect(mapStateProps, {authUser})(Main));
+export default withRouter(connect(mapStateProps, { authUser })(Main));
--- FinalProject/warbler/warbler-client/src/store/index.js
+++ FinalProject/warbler/warbler-client/src/store/index.js
@@ -1,14 +1,14 @@
 import rootReducer from "./reducers";
-import {createStore, applyMiddleware, compose} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 export function configureStore() {
-    const store = createStore(
-        rootReducer, 
-        compose(
-            applyMiddleware(thunk),
-            window.devToolsExtension ? window.devToolsExtension() : f => f 
-        )
-    );
-    return store;
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
+  return store;
 }

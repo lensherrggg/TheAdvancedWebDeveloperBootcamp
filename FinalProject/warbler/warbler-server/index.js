e1a470524bab133114c1c51f53f7d0ce45272e1c
lensherrggg <gr98522@gmail.com>
2020-03-17 16:34:55 +0800

    final project backend finished

--- FinalProject/warbler/warbler-server/index.js
+++ FinalProject/warbler/warbler-server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const {loginRequired, ensureCorrectUser} = require("./middleware/auth");
 
 const PORT = 8081;
 
@@ -12,6 +14,25 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", 
+    loginRequired, 
+    ensureCorrectUser, 
+    messagesRoutes
+);
+
+app.get("/api/messages", loginRequired, async function(req, res, next) {
+    try {
+        let messages = await db.Message.find()
+            .sort({createAt: "desc"})
+            .populate("user", {
+                username: true,
+                profileImageUrl: true
+            });
+        return res.status(200).json(messages);
+    } catch(e) {
+        return next(e);
+    }
+})
 // all my routes over here - they will come later!
 
 app.use(function(req, res, next) {
--- FinalProject/warbler/warbler-server/models/index.js
+++ FinalProject/warbler/warbler-server/models/index.js
@@ -3,7 +3,7 @@ mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/warbler", {
     keepAlive: true,
-    useMongoClient: true
 });
 
 module.exports.User = require("./user");
+module.exports.Message = require("./message")
